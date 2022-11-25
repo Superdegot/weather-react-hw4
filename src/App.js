669b59600ec4f1491dd1e1a9134d43ac0bbd6b23
Superdegot <65909405+Superdegot@users.noreply.github.com>
2022-11-25 12:32:46 +0000

    Added link to github

--- src/App.js
+++ src/App.js
@@ -7,6 +7,14 @@ function App() {
     <div className="App">
       <h1>Weather App</h1>
       <Search />
+      <footer>
+        <a
+          href="https://github.com/Superdegot/weather-react-hw4"
+          target="_blank"
+        >
+          Open-source code by Ekaterina Filatova
+        </a>
+      </footer>
     </div>
   );
 }
